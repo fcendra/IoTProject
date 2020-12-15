1d54f2d03e6ab6ed647b2bcbb445c44dfda51491
fcendra <fcendra12@gmail.com>
2020-12-16 04:08:46 +0800

    add gitignore

--- Day 5/submission/face_datasets.py
+++ Day 5/submission/face_datasets.py
@@ -11,7 +11,7 @@ while(True):
     _, image_frame = vid_cam.read()
     gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
         count += 1
         cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
